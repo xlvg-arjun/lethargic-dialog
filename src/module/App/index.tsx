99abfa78fa86d59065a38aeb3cdb3dde0bf0e57a
Arjun Raju Pillai <revarjunr@gmail.com>
2020-04-20 11:57:38 +0530

    Demonstrate features

--- src/module/App/index.tsx
+++ src/module/App/index.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../../lib/core';
 import IconDialog from '../../lib/components/IconDialog';
+import Actions from '../../lib/types/Actions';
+import Icons from '../../lib/types/Icons';
 
 function App() {
+  const [dialogOpen, set_dialogOpen] = useState(true);
+  const closeDialog = () => { set_dialogOpen(false); };
   return (
     <main>
-      <IconDialog />
+      {dialogOpen && <IconDialog Title='Titular' Description='Foolproof Description' IconType={Icons.Success} closeDialog={closeDialog} actions={
+        [
+          { ActionType: Actions.Success, Label: 'PRotect Everyone', Action: () => {}, KeepAlive: true },
+          { ActionType: Actions.Confirm, Label: 'Protect', Action: () => {}, KeepAlive: true },
+          { ActionType: Actions.Danger, Label: 'Close', Action: (closingFnProvided: Function) => { closingFnProvided() }, KeepAlive: false }
+        ]
+      }/>}
     </main>
   );
 }
